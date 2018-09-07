074f08e535478234ec18165da0c2b0eb3b9a937e
Arthur Bates <aabatesj@gmail.com>
2018-09-07 13:42:30 -0400

    starting Routes
    adding routes to server

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const server = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const projectRoutes = require('./Routes/projectRoutes');
+const actionRoutes = require('./Route/actionRoutes');
+
 server.use(cors());
 server.use(bodyParser.json());
+server.use('/project', projectRoutes);
+server.use('/action', actionRoutes);
 
 server.get('/', (req, res) => {
     res.send('successful');
